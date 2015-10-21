041a190bbb560270c9b674e92a648c9739d13663
Alexandr <advokat-1987@yandex.ru>
2015-10-22 00:18:50 +0300

    9

--- controler/registration.php
+++ controler/registration.php
@@ -1,17 +1,15 @@
 <?php
-require_once __DIR__ . '/../classes/sql.php';
+
 require_once __DIR__.'/../autoload.php';
 $l = new Login();
 $log = $_POST['login'];
 $pas = $_POST['pas'];
-$logins = $l->findAll();
+$logins = $l::findAll();
 session_start();
 var_dump($logins);
 
 foreach ($logins->items as $item) {
-    echo $item;
-
-
+    echo $item->login;
     /*if ($item->login == $log) {
         $_SESSION['error'] = 'Такой логин уже существует!';
         header('Location: ../views/regist.php');
